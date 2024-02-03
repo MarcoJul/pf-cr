7ab903769178bd79fc571b66cb0cf393d3af09f3
Marco <marco.giuliotti@gmail.com>
2024-02-03 14:46:23 +0100

    Add navigation component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,11 @@
-import Image from "next/image";
 import styles from "./page.module.css";
-import Link from "next/link";
+import Navigation from "@/components/Navigation";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       Homepage
-      <nav className={styles.navigation}>
-            <ul className={styles.menu}>
-                    <Image src="/MenuIcon.svg" className={styles.menuIcon} alt="HomePage Icon" width={48} height={48} />
-                <li className={styles.active}>
-                    <Link className={styles.navLink} href="/projects">Projects</Link>
-                </li>
-                <li>
-                    <Link className={styles.navLink}  href="/method">Method</Link>
-                </li>
-                <li>
-                    <Link  className={styles.navLink} href="/about">About</Link>
-                </li>
-            </ul>
-        </nav>
+      <Navigation />
     </main>
   );
 }
---
+++ src/components/Navigation/index.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./navigation.module.css";
+
+export default function Navigation() {
+  return (
+    <nav className={styles.navigation}>
+      <ul className={styles.menu}>
+        <li>
+          <Link href="/">
+            <Image
+              src="/MenuIcon.svg"
+              className={styles.menuIcon}
+              alt="Go to homepage"
+              width={48}
+              height={48}
+            />
+          </Link>
+        </li>
+        <li className={styles.active}>
+          <Link className={styles.navLink} href="/projects">
+            Projects
+          </Link>
+        </li>
+        <li>
+          <Link className={styles.navLink} href="/method">
+            Method
+          </Link>
+        </li>
+        <li>
+          <Link className={styles.navLink} href="/about">
+            About
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
