26a889dfaec3f251368ca26b0806898d11337216
Marco <marco.giuliotti@gmail.com>
2024-02-04 15:05:14 +0100

    Add story section and test intersection observer

--- src/app/projects/kiko/page.tsx
+++ src/app/projects/kiko/page.tsx
@@ -1,3 +1,4 @@
+import Story from "./components/story";
 import styles from "./page.module.css";
 
 export default function Home() {
@@ -6,9 +7,7 @@ export default function Home() {
       <section id="hero" className={styles.hero}>
         <h1 className={styles.title}>KIKO Milano</h1>
       </section>
-      <section id="story" className={styles.storySection}>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus, exercitationem assumenda a illum optio non consequuntur neque sapiente animi omnis? Quod magni illum, excepturi neque dolore amet iure molestias obcaecati.
-      </section> 
+      <Story />
     </main>
   );
 }
