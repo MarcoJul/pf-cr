f56e2ac6a3519e6960735ed2c820c99120cb73e5
Marco <marco.giuliotti@gmail.com>
2024-02-04 13:12:37 +0100

    Add basic structure for kiko landing section

--- src/app/projects/kiko/page.tsx
+++ src/app/projects/kiko/page.tsx
@@ -1,17 +1,14 @@
 import styles from "./page.module.css";
-import Link from "next/link";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      Kiko
-      <nav className={styles.navigation}>
-            <ul className={styles.menu}>
-                <li className={styles.active}>
-                    <Link className={styles.navLink} href="/projects">Back to projects</Link>
-                </li>
-            </ul>
-        </nav>
+      <section id="hero" className={styles.hero}>
+        <h1 className={styles.title}>KIKO Milano</h1>
+      </section>
+      <section id="story" className={styles.storySection}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus, exercitationem assumenda a illum optio non consequuntur neque sapiente animi omnis? Quod magni illum, excepturi neque dolore amet iure molestias obcaecati.
+      </section> 
     </main>
   );
 }
