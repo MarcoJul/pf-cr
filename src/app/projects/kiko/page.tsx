43f4465ebfbd15ca61bef4671571cf1a2c7e80e8
Marco <marco.giuliotti@gmail.com>
2024-02-04 17:08:52 +0100

    Add content to test kiko landing page

--- src/app/projects/kiko/page.tsx
+++ src/app/projects/kiko/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Story from "./components/story";
 import styles from "./page.module.css";
 
@@ -5,8 +6,14 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <section id="hero" className={styles.hero}>
-        <h1 className={styles.title}>KIKO Milano</h1>
+        <div className={styles.heroTextBox}>
+          <h1 className={styles.title}>KIKO Milano</h1>
+          <p className={styles.kicker}>Composable Commerce:<br/> a continuous evolution</p>
+        </div>
       </section>
+      <div className={styles.boxImage}>
+        <Image className={styles.coverImage} alt="kiko intro image" width={525} height={525} src="/kiko/intro-kiko.jpg" priority/>
+      </div>
       <Story />
     </main>
   );
