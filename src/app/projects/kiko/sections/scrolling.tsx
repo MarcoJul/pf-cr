67569d255fe6bfa2e59b2849b2bbd8f0fbf9f75e
Marco <marco.giuliotti@gmail.com>
2024-02-20 20:05:13 +0100

    Add kiko logo and basic structure for scrolling text section

--- src/app/projects/kiko/sections/scrolling.tsx
+++ src/app/projects/kiko/sections/scrolling.tsx
@@ -1,10 +1,20 @@
 
+import Image from "next/image";
 import styles from "./scrolling.module.css"
 
 export default function Scrolling() {
   return (
-    <section className={styles.dataSection}>
-      Scrolling Text
+    <section className={styles.scrollingSection}>
+      <div className={styles.marquee}>
+      <Image
+        className={styles.logo}
+					alt='kiko-logo'
+					width={237}
+					height={135}
+					src="/kiko/logo-kiko.svg"
+					priority
+			/>
+     </div>
     </section>
   );
 }
