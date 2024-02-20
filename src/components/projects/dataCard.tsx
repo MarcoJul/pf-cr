b5a17375c7ce34a36e9914e639f5604f533acffa
Marco <marco.giuliotti@gmail.com>
2024-02-20 19:54:26 +0100

    Modify width for embed spotify tool

--- src/components/projects/dataCard.tsx
+++ src/components/projects/dataCard.tsx
@@ -34,7 +34,7 @@ export default function DataCard({icon, title, text, isTextWebsite, href, isMusi
 				</div>
 			}
 			{isMusic && 
-				<iframe style={{borderRadius: "12px", border: "0px"}} src="https://open.spotify.com/embed/track/5vaOdnTldKw74qWjezLfRO?utm_source=generator&theme=0" width="80%" height="100" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+				<iframe style={{borderRadius: "12px", border: "0px"}} src="https://open.spotify.com/embed/track/5vaOdnTldKw74qWjezLfRO?utm_source=generator&theme=0" width="100%" height="100" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
 			}
 		</li>
 	);
