ae5850de3984872cd1d64bce9ccc303506b37ec9
Marco <marco.giuliotti@gmail.com>
2024-02-21 22:24:52 +0100

    Rename variables and refactoring

--- src/app/projects/kiko/sections/containerWrapper.tsx
+++ src/app/projects/kiko/sections/containerWrapper.tsx
@@ -5,61 +5,63 @@ import Intro from './intro';
 import Scrolling from './scrolling';
 
 export default function ContainerWrapper() {
-  const [contentIsFullPage, setContentIsFullPage] = useState<boolean>(false);
+	const [contentIsFullPage, setContentIsFullPage] = useState<boolean>(false);
 
-  const handleScroll = () => {
-    const position = window.scrollY;
-    const windowHeight = window.innerHeight;
+	const handleScroll = () => {
+		const position = window.scrollY;
+		const windowHeight = window.innerHeight;
 
-    const totalWidth = window.innerWidth;
-    console.log(position, totalWidth, windowHeight);
+		const introTextContent:HTMLElement = document.querySelector('#content')!;
+		const introImageContent: HTMLElement = document.querySelector('#image-content')!;
 
 
-    if(windowHeight - 100 < position && position < 5000){
-      setContentIsFullPage(true);
-    } else {
-      setContentIsFullPage(false);
-    }
-    const obj:HTMLElement = document.querySelector('#content')!;
-    const obj2: HTMLElement = document.querySelector('#image-content')!;
-    if(position >= windowHeight && position < windowHeight * 2 - 100){
-      // console.log('translate', position, window.innerWidth);
-      console.log('translate');
-      obj.style.transition = "none";
-      obj2.style.transition= "none";
-      obj.style.transform = 'translateX(-' + (position - windowHeight) + 'px)';
-      obj2.style.transform = 'translateX(-' + (position - windowHeight) + 'px)';
-    } else if (position < 1450){
-      obj.style.transform = 'translateX(0px)';
-      obj2.style.transform = 'translateX(0px)';
-      obj.style.transition = "all .5s";
-      obj2.style.transition= "all .5s";
-    }
-  }
+		if(windowHeight - 100 < position && position < 5000){
+			setContentIsFullPage(true);
+		} else {
+			setContentIsFullPage(false);
+		}
+
+		if(position >= windowHeight && position < windowHeight * 2 - 100){
+			introTextContent.style.transition = "none";
+			introImageContent.style.transition = "none";
+			introTextContent.style.transform = 'translateX(-' + (position - windowHeight) + 'px)';
+			introImageContent.style.transform = 'translateX(-' + (position - windowHeight) + 'px)';
+		} else if (position < 1450){
+			introTextContent.style.transform = 'translateX(0px)';
+			introImageContent.style.transform = 'translateX(0px)';
+			introTextContent.style.transition = "all .5s";
+			introImageContent.style.transition = "all .5s";
+		}
+	}
 
 useEffect(() => {
 
-  const position = window.scrollY;
-  const windowHeight = window.innerHeight;
+	const position = window.scrollY;
+	const windowHeight = window.innerHeight;
 
-  if(windowHeight - 100 < position && position < 5000){
-    setContentIsFullPage(true);
-  } else {
-    setContentIsFullPage(false);
-  }
+	const introTextContent:HTMLElement = document.querySelector('#content')!;
+	const introImageContent: HTMLElement = document.querySelector('#image-content')!;
 
-  window.addEventListener('scroll', handleScroll, { passive: true });
+	if(windowHeight - 100 < position && position < 5000){
+		setContentIsFullPage(true);
+		introTextContent.style.transform = 'translateX(-' + (windowHeight-100) + 'px)';
+		introImageContent.style.transform = 'translateX(-' + (windowHeight-100) + 'px)';
+	} else {
+		setContentIsFullPage(false);
+	}
 
-  return () => {
-      window.removeEventListener('scroll', handleScroll);
-  };
+	window.addEventListener('scroll', handleScroll, { passive: true });
+
+	return () => {
+			window.removeEventListener('scroll', handleScroll);
+	};
 }, []);
 
-  return (
-    <div className={`${styles.containerWrapper} ${contentIsFullPage? styles.fullPage : ''}`}>
-      <Intro isFullPage={contentIsFullPage}/>
-      <Scrolling/>
-      <div className={styles.emptyBox}></div>
-    </div>
-  )
+	return (
+		<div className={`${styles.containerWrapper} ${contentIsFullPage? styles.fullPage : ''}`}>
+			<Intro isFullPage={contentIsFullPage}/>
+			<Scrolling/>
+			<div className={styles.emptyBox}></div>
+		</div>
+	)
 }
