9f1c68fb11431a0cbc64cc5d181d4f33aae2429f
Marco <marco.giuliotti@gmail.com>
2024-02-28 21:49:02 +0100

    Insert media section with small video

--- src/app/projects/kiko/sections/mobileMediaSection.tsx
+++ src/app/projects/kiko/sections/mobileMediaSection.tsx
@@ -9,17 +9,31 @@ interface IMobileMediaSection {
 
 export default function MobileMediaSection ({image1, image2, image3}: IMobileMediaSection) {
 
-
 	return (
 	<div className={styles.container}>
 		<div className={styles.imageBox}>
-			<Image
-				className={styles.image}
-				alt="kiko intro image"
-				width={216}
-				height={466.5}
-				src={image1}
-			/>
+			{image1.includes('.mp4') ?
+				<video 
+					className={styles.image}
+					width="216"
+					height="466.5"
+					controls
+					poster="/kiko/storyline/2020_app1.jpg"
+					preload="none"
+				>
+				<source src={image1}
+				type="video/mp4"
+				/> Your browser does not support the video tag.
+				</video>
+				:
+				<Image
+					className={styles.image}
+					alt="kiko intro image"
+					width={216}
+					height={466.5}
+					src={image1}
+				/>
+			}
 			<Image
 				className={styles.image}
 				alt="kiko intro image"
