38c647b93a4906186d29eb52fcf243c53a315098
Marco <marco.giuliotti@gmail.com>
2024-03-05 19:47:22 +0100

    Change source for svg arrow icon

--- src/components/nav/anchorMenu.tsx
+++ src/components/nav/anchorMenu.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import styles from './anchorMenu.module.css'
 
 interface IAnchorMenuProps {
@@ -7,16 +6,19 @@ interface IAnchorMenuProps {
 	thirdActive: boolean;
 }
 
-function IconArrow () {
-	return <Image
-			src="/shared/icon-arrow-right.svg"
-			className={styles.arrowIcon}
-			alt=""
-			width={30}
-			height={30}
-			priority
-		/>
-}
+const ArrowIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={48}
+    height={48}
+    fill="none"
+		className={styles.arrowIcon}
+  >
+    <path
+      d="M28.79 34c.195-.962.52-2.003.977-3.125a15.076 15.076 0 0 1 1.906-3.27 8.76 8.76 0 0 1 2.932-2.403v-.192L11 25.298v-2.596l23.605.288v-.192c-1.205-.737-2.199-1.602-2.98-2.596a13.438 13.438 0 0 1-1.858-3.125A23.538 23.538 0 0 1 28.79 14h2.883c.163 1.154.456 2.276.88 3.365a14.994 14.994 0 0 0 1.759 3.03A16.23 16.23 0 0 0 37 23.086v1.826c-1.499 1.25-2.704 2.58-3.617 3.99-.88 1.411-1.45 3.11-1.71 5.097H28.79Z"
+    />
+  </svg>
+)
 
 export default function AnchorMenu ({firstActive, secondActive, thirdActive}: IAnchorMenuProps) {
 
@@ -26,7 +28,7 @@ export default function AnchorMenu ({firstActive, secondActive, thirdActive}: IA
 
 				<div className={`${styles.text} ${firstActive ? styles.activeBox: ''}`}>
 					<a href="#ecommerce-redesign" className={styles.titleBlock}>
-						<IconArrow />
+						<ArrowIcon />
 						<h3 className={styles.mainTitle}>2018</h3>
 						<span className={styles.lineSeparator}></span>
 						<h3 className={styles.mainTitle}>19</h3>
@@ -36,7 +38,7 @@ export default function AnchorMenu ({firstActive, secondActive, thirdActive}: IA
 
 				<div className={`${styles.text} ${secondActive ? styles.activeBox: ''}`}>
 					<a href="#evolutive-roadmap" className={styles.titleBlock}>
-						<IconArrow />
+						<ArrowIcon />
 						<h3 className={styles.mainTitle}>2020</h3>
 						<span className={styles.lineSeparator}></span>
 						<h3 className={styles.mainTitle}>22</h3>
@@ -46,7 +48,7 @@ export default function AnchorMenu ({firstActive, secondActive, thirdActive}: IA
 
 				<div className={`${styles.text} ${thirdActive ? styles.activeBox: ''}`}>
 					<a href="#unified-commerce" className={styles.titleBlock}>
-						<IconArrow />
+						<ArrowIcon />
 						<h3 className={styles.mainTitle}>2023</h3>
 						<span className={styles.lineSeparator}></span>
 						<h3 className={styles.mainTitle}>24</h3>
